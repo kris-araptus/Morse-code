6afdfa6a9043c3c0e361a7cac05503c70cd2cc65
Kristopher Black <Kris.developer@gmail.com>
2024-06-28 00:18:28 -0500

    init commit

---
+++ morsecode.py
@@ -0,0 +1,34 @@
+# Morse code dictionary
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', 
+    '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', 
+    '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', 
+    '$': '...-..-', '!': '-.-.--', '@': '.--.-.', ' ': '/'
+}
+
+# Function to convert text to Morse code
+def text_to_morse_code(text):
+    morse_code = ''
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char] + ' '
+        else:
+            morse_code += ' '
+    return morse_code
+
+# Get the paragraph from the user
+paragraph = input("Enter a paragraph: ")
+
+# Convert the paragraph to Morse code
+morse_code = text_to_morse_code(paragraph)
+
+# Print the Morse code
+print(morse_code)
+
